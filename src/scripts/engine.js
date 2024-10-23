3b175da2ad02e2c2b64ca4707f5005c4d9b0988b
Endryl Richard Monteiro <endrylrm@hotmail.com>
2024-10-23 16:58:30 -0300

    feat: engine do jogo.

---
+++ src/scripts/engine.js
@@ -0,0 +1,61 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+    lives: document.querySelector("#lives"),
+  },
+  values: {
+    timerId: null,
+    countDownTimerId: setInterval(countDown, 1000),
+    gameSpeed: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
+  },
+};
+
+function countDown() {
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+
+  if (state.values.currentTime <= 0) {
+    alert("Game Over! o seu resultado foi: " + state.values.result);
+  }
+}
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
+}
+
+function moveEnemy() {
+  state.values.timerId = setInterval(randomSquare, state.values.gameSpeed);
+  addListenerHitbox();
+}
+
+function addListenerHitbox() {
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+      }
+    });
+  });
+}
+
+function initialize() {
+  moveEnemy();
+  addListenerHitbox();
+}
+
+initialize();
